29cec9472494c449d741c3b30a3838a36b68b9ed
unknown <goyalaayush93@gmail.com>
2022-10-17 19:06:42 +0530

    Team Positions Model

--- scisoc/views.py
+++ scisoc/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.contrib import messages
 from .models import LatestNews
+from user.models import SocHeads
 
 def homepage(request):
     # latest news rendering
-    news=LatestNews.objects.all().order_by('date_time')
+    news = LatestNews.objects.all().order_by('date_time')
+    # Team Positions rendering
+    team = SocHeads.objects.all().order_by('position__value')
     # HomePage Render
-    return render(request,'homepage.html',{'news':news})
+    return render(request,'homepage.html',{'news':news,'team':team})
--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
+from distutils.command.upload import upload
+from email.policy import default
+from operator import mod
+from pickle import TRUE
+from turtle import position
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Positions(models.Model):
+    position = models.CharField(default="MEMBER",max_length=25,unique=True)
+    value = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.position
+
+class SocHeads(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    year = models.IntegerField(default=1)
+    position = models.ForeignKey(Positions,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='headimages/',null=True,blank=True)
+
+    def __str__(self):
+        return self.user.first_name
